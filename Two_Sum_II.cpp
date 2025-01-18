051eefa6f8df452f2e818b2f63d8f621e47306e0
Aravindh Balaji <142385502+aravindhbalaji04@users.noreply.github.com>
2025-01-18 22:57:13 +0530

    Update Two_Sum_II.cpp

--- Two_Sum_II.cpp
+++ Two_Sum_II.cpp
@@ -1,11 +1,14 @@
 class Solution {
 public:
     vector<int> twoSum(vector<int>& nums, int target) {
-        int i=0, j=nums.size()-1;
+        int n=nums.size();
+        int i=0;
+        int j=n-1;
         while(i<j){
-            if(nums[i]+nums[j] > target){
+            int sum = nums[i]+nums[j];
+            if(sum > target){
                 j--;
-            }else if(nums[i]+nums[j] < target){
+            }else if(sum < target){
                 i++;
             }else{
                 return {i+1,j+1};
