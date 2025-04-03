03414a65ba37354f4d92fbeb8991dfe28b127285
Aravindh Balaji <142385502+aravindhbalaji04@users.noreply.github.com>
2025-04-03 14:02:11 +0530

    Create file

---
+++ 69-Sqrt(x)/69-Sqrt(x).py
@@ -0,0 +1,21 @@
+# Last updated: 3/4/2025, 2:02:10 pm
+class Solution:
+    def mySqrt(self, x: int) -> int:
+        if x == 0 or x == 1:
+            return x
+        
+        start, end = 0, x
+        ans = 0
+        
+        while start <= end:
+            mid = (start + end) // 2
+            
+            if mid * mid == x:
+                return mid
+            elif mid * mid < x:
+                ans = mid  
+                start = mid + 1
+            else:
+                end = mid - 1
+        
+        return ans
